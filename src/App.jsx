3e4cc5c79ede4e5f97d7c98ea44d22a631297741
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2022-12-29 23:11:22 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// import logo from './logo.svg';
+import "./App.css";
+import { Counter } from "./features/counter/counter";
+
+function App() {
+  return <Counter></Counter>;
+}
+
+export default App;
